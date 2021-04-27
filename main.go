ca886b20e902d215952db006f7481ca8559f842b
Tom Victor <vjtomvictor@icloud.com>
2021-04-27 20:52:35 +0530

    arrays

--- main.go
+++ main.go
@@ -49,4 +49,16 @@ func main() {
 	fmt.Println(pi + 1.2)
 
 	fmt.Println(PUBLISHED, DRAFT, TRASH)
+
+	var arr [3]int
+
+	arr[0] = 1
+	arr[1] = 2
+	arr[2] = 3
+
+	fmt.Println(arr)
+
+	arr2 := [3]int{1, 2, 3}
+
+	fmt.Println(arr2)
 }
