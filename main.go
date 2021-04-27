917ac6fdd7932c2749a59d8110284b85e7ea4779
Tom Victor <vjtomvictor@icloud.com>
2021-04-27 20:02:25 +0530

    data type declaration

--- main.go
+++ main.go
@@ -3,5 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Helo World")
+	var i int
+	i = 42
+	fmt.Println(i)
+
+	var f float32 = 3.14
+	fmt.Println(f)
+
+	firstName := "Tom"
+	fmt.Println(firstName)
+
+	b := true
+
+	fmt.Println(b)
+
+	c := complex(3, 4)
+	fmt.Println(c)
+
+	r, im := real(c), imag(c)
+	fmt.Println(r, im)
 }
