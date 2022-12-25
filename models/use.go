61a0272e0100a2ecffd0c56df6153470a1ac5c75
Tom Victor <vjtomvictor@icloud.com>
2022-12-25 16:23:23 +0100

    crud funcs

--- models/use.go
+++ models/use.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"errors"
+	"fmt"
+)
+
 type User struct {
 	ID        int
 	FirstName string
@@ -16,8 +21,40 @@ func GetUsers() []*User {
 }
 
 func AddUser(u User) (User, error) {
+	if u.ID != 0 {
+		return User{}, errors.New("new user must not include ID")
+	}
 	u.ID = nextID
 	nextID++
 	users = append(users, &u)
 	return u, nil
+}
+
+func GetUserByID(id int) (User, error) {
+	for _, u := range users {
+		if u.ID == id {
+			return *u, nil
+		}
+	}
+	return User{}, fmt.Errorf("no user found with id %v", id)
+}
+
+func UpdateUserByID(u User) (User, error) {
+	for i, candidate := range users {
+		if u.ID == candidate.ID {
+			users[i] = &u
+			return u, nil
+		}
+	}
+	return User{}, fmt.Errorf("no user found with id %v", u.ID)
+}
+
+func RemoveUserByID(id int) error {
+	for i, u := range users {
+		if u.ID == id {
+			users = append(users[:i], users[i+1:]...)
+			return nil
+		}
+	}
+	return fmt.Errorf("no user found with id %v", id)
 }
