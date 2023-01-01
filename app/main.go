5a6c48a3097d523ec8b63c0d618d16566b4dcf84
Tom Victor <vjtomvictor@icloud.com>
2023-01-01 21:09:38 +0100

    refactoring

--- app/main.go
+++ app/main.go
@@ -7,53 +7,45 @@ import (
 	"strings"
 )
 
-func main() {
-	type menuItem struct {
-		name   string
-		prices map[string]float64
-	}
+type menuItem struct {
+	name   string
+	prices map[string]float64
+}
 
-	menu := []menuItem{
-		{
-			name: "Coffe",
-			prices: map[string]float64{
-				"regular": 12.5,
-				"large":   20,
-			},
+var menu = []menuItem{
+	{
+		name: "Coffe",
+		prices: map[string]float64{
+			"regular": 12.5,
+			"large":   20,
 		},
-		{
-			name: "tea",
-			prices: map[string]float64{
-				"single": 10,
-				"double": 15,
-				"triple": 20,
-			},
+	},
+	{
+		name: "tea",
+		prices: map[string]float64{
+			"single": 10,
+			"double": 15,
+			"triple": 20,
 		},
-	}
+	},
+}
+
+var in = bufio.NewReader(os.Stdin)
+
+func main() {
+
 loop:
 
 	for {
+		printInfo()
 
-		in := bufio.NewReader(os.Stdin)
-
-		fmt.Println("Please select an option")
-		fmt.Println("1) Display menu")
-		fmt.Println("2) Add item")
-		fmt.Println("q) quit")
 		choice, _ := in.ReadString('\n')
 
 		switch strings.TrimSpace(choice) {
 		case "1":
-			for _, item := range menu {
-				fmt.Println(item.name)
-				fmt.Println(strings.Repeat("-", 10))
-				for key, value := range item.prices {
-					fmt.Printf("\t%10s%10.2f\n", key, value)
-				}
-			}
+			printMenu()
 		case "2":
-			name, _ := in.ReadString('\n')
-			menu = append(menu, menuItem{name: name, prices: make(map[string]float64)})
+			addItem()
 		case "q":
 			break loop
 		default:
@@ -61,4 +53,26 @@ loop:
 		}
 	}
 
+}
+
+func printMenu() {
+	for _, item := range menu {
+		fmt.Println(item.name)
+		fmt.Println(strings.Repeat("-", 10))
+		for key, value := range item.prices {
+			fmt.Printf("\t%10s%10.2f\n", key, value)
+		}
+	}
+}
+
+func addItem() {
+	name, _ := in.ReadString('\n')
+	menu = append(menu, menuItem{name: name, prices: make(map[string]float64)})
+}
+
+func printInfo() {
+	fmt.Println("Please select an option")
+	fmt.Println("1) Print menu")
+	fmt.Println("2) Add item")
+	fmt.Println("q) quit")
 }
