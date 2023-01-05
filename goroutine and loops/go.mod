6119a2b8e1c4e4ce6af60bf211940b51f6817b52
Tom Victor <vjtomvictor@icloud.com>
2023-01-05 01:08:35 +0100

    goroutine and loops

---
+++ goroutine and loops/go.mod
@@ -0,0 +1,3 @@
+module goroutineloops
+
+go 1.19
---
+++ goroutine and loops/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	ch := make(chan int)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			ch <- i
+		}
+		close(ch)
+	}()
+
+	for msg := range ch {
+		fmt.Println(msg)
+	}
+}
