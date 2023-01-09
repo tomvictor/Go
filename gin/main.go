0df4903ea2120e898591649695b611743cfe0386
Tom Victor <vjtomvictor@icloud.com>
2023-01-10 00:14:33 +0100

    updated gin routes

--- gin/main.go
+++ gin/main.go
@@ -1,25 +1,17 @@
 package main
 
 import (
-	"embed"
 	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-// go:embed public/*
-var f embed.FS
-
 func main() {
 	router := gin.Default()
 
-	router.StaticFile("/", "./public/index.html")
-	router.Static("/static", "./public")
-	router.StaticFS("/fs", http.FileSystem(http.FS(f)))
-
 	router.GET("/employee", func(c *gin.Context) {
-		c.File("./public/index.html")
+		c.File("./public/employee.html")
 	})
 	router.POST("employee", func(c *gin.Context) {
 		c.String(http.StatusOK, "New Request accepted")
