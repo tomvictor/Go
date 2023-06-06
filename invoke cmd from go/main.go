d50b3e948bf4c304daf24492486cba89ec87e615
Tom Victor <vjtomvictor@icloud.com>
2023-06-06 10:05:58 +0200

    Update main.go

--- invoke cmd from go/main.go
+++ invoke cmd from go/main.go
@@ -3,9 +3,31 @@ package main
 import (
 	"fmt"
 	"os/exec"
+	"time"
 )
 
 func main() {
+
+	msgCh := make(chan string)
+
+	go FetchFromCli(msgCh)
+	go PrintOutCli(msgCh)
+
+	time.Sleep(time.Minute)
+
+}
+
+func PrintOutCli(msgCh chan string) {
+	var data string
+	for {
+		select {
+		case data = <-msgCh:
+			fmt.Println(data)
+		}
+	}
+}
+
+func FetchFromCli(msg chan string) {
 	cli := exec.Command("./sensor/sensor")
 	pipe, err := cli.StdoutPipe()
 	if err != nil {
@@ -19,6 +41,6 @@ func main() {
 	buff := make([]byte, 20)
 	for {
 		pipe.Read(buff)
-		fmt.Println(string(buff))
+		msg <- string(buff)
 	}
 }
